3df5b2311f58a6ddc745964cdf31e84e64220f88
Lucas Mangelo <lucas.mangelo@mag-it.com.br>
2019-05-14 22:08:26 -0300

    criação de modelos iniciais do projeto

--- syscon/app/models.py
+++ syscon/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Function(models.Model):
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+class Collaborator(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    birth_day = models.DateTimeField()
+    function = models.ForeignKey('Function')
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
