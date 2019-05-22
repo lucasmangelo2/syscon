a008fdb8b7484786082659b11ad61c1325711068
lucas.mangelo <lucas.mangelo@yahoo.com.br>
2019-05-21 22:47:26 -0300

    primeira migration e configurações

--- syscon/app/admin.py
+++ syscon/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Function, Collaborator
 
 # Register your models here.
+
+admin.site.register(Function)
+admin.site.register(Collaborator)
--- syscon/app/models.py
+++ syscon/app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+
+# Create your models here.
+from django.db import models
 from django.utils import timezone
 # Create your models here.
 
@@ -16,7 +19,7 @@ class Collaborator(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     birth_day = models.DateTimeField()
-    function = models.ForeignKey('Function')
+    function = models.ForeignKey('Function', on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
 
     def publish(self):
