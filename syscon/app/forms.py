7694432312e977b61f53c51d3378c77646f717df
Lucas Mangelo <lucas.mangelo@mag-it.com.br>
2019-05-28 22:17:17 -0300

    alterações no formulário

--- syscon/app/forms.py
+++ syscon/app/forms.py
@@ -2,20 +2,24 @@ from django import forms
 from .models import Function, Collaborator
 
 class FunctionForm(forms.ModelForm):
-
     class Meta:
         model = Function
         fields = ('name',)
 
 
-class CollaboratorForm(forms.ModelForm):
-    class Meta:
-        model = Collaborator
-        fields = ('name','birth_day','function')
+class CollaboratorForm(forms.Form):
+    name = forms.CharField(
+        max_length=200, 
+        required=True, 
+        widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+    birth_day = forms.DateField(
+        widget=forms.widgets.DateInput(format="%dd/%mm/%YYYY", attrs={'class':'form-control', 'type':'date'}),
+        required=True)
 
-    ''' model = Collaborator
-    name = forms.CharField(max_length=200, required=True)
-    birth_day = forms.DateField(widget=forms.widgets.DateInput(format="%d/%m/%Y"),required=True)
-    function = forms.ModelChoiceField(queryset=Function.objects.all(), widget=forms.Select, required=True)
-    '''
+    function = forms.ModelChoiceField(
+        queryset=Function.objects.all(), 
+        widget=forms.Select(attrs={'class':'form-control'}), 
+        required=True)
+    
     
